fab57c12b3462b6ddaa2744f8f876998912076f6
Illumiy <dima.skobelev.1979@mail.ru>
2019-09-20 12:35:57 +0300

    novaya vetka test

--- config/web.php
+++ config/web.php
@@ -22,10 +22,7 @@ $config = [
         ],
     ],
     'components' => [
-        'authManager' => [
-            'class' => 'yii\rbac\AuthorRule',
-            'defaultRoles' => ['admin', 'BRAND', 'TALENT'], // Здесь нет роли "guest", т.к. эта роль виртуальная и не присутствует в модели UserExt
-        ],
+//
         'authClientCollection' => [
             'class'   => \yii\authclient\Collection::className(),
             'clients' => [
